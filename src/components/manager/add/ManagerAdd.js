9736fd5c70e56662efd533364c1a033a6b95ae84
arsay <arsay@foxmail.com>
2018-04-24 12:04:30 +0800

    separate residences;

--- src/components/manager/add/ManagerAdd.js
+++ src/components/manager/add/ManagerAdd.js
@@ -3,45 +3,12 @@ import BreadcrumbCustom from '../../BreadcrumbCustom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchData, receiveData } from '@/action';
-
+import { residences } from '../../../constants/residences';
 import { Form, Input, Tooltip, Icon, Cascader, Select, Button, AutoComplete, message } from 'antd';
 
 const FormItem = Form.Item;
 const Option = Select.Option;
 const AutoCompleteOption = AutoComplete.Option;
-
-const residences = [{
-    value: '陕西省',
-    label: '陕西省',
-    children: [{
-        value: '西安市',
-        label: '西安市',
-        children: [{
-            value: '长安区',
-            label: '长安区',
-        },{
-            value: '雁塔区',
-            label: '雁塔区',
-        },{
-            value: '浐灞区',
-            label: '浐灞区',
-        }],
-    }],
-}, {
-    value: '上海市',
-    label: '上海市',
-    children: [{
-        value: '青浦区',
-        label: '青浦区',
-    },{
-        value: '徐汇区',
-        label: '徐汇区',
-    },{
-        value: '金山区',
-        label: '金山区',
-    }]
-}
-];
 
 const emailSuffix = ['qq.com', 'gmail.com', '163.com'];
 
